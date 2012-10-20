5bd53725d8e57a683ea4019181eaa4188a13ccc9
Yanick Champoux <yanick@cpan.org>
2012-10-20 16:43:39 -0400

    Build results of 70bbcbe (on master)

--- lib/File/ShareDir/Tarball.pm
+++ lib/File/ShareDir/Tarball.pm
@@ -3,7 +3,7 @@ BEGIN {
   $File::ShareDir::Tarball::AUTHORITY = 'cpan:YANICK';
 }
 {
-  $File::ShareDir::Tarball::VERSION = '0.1.0';
+  $File::ShareDir::Tarball::VERSION = '0.2.0';
 }
 # ABSTRACT: Deal transparently with shared files distributed as tarballs
 
@@ -21,18 +21,27 @@ use File::Temp qw/ tempdir /;
 use File::chdir;
 
 our @EXPORT_OK   = qw{
-    dist_dir
+    dist_dir dist_file
 };
 our %EXPORT_TAGS = (
-    ALL => [ @EXPORT_OK ],
+    all => [ @EXPORT_OK ],
 );
 
 my $shared_files_tarball = 'shared-files.tar.gz';
 
-sub dist_dir {
-    my $dir = File::ShareDir::dist_dir(@_);
+# we don't want to extract the same dirs again and 
+# again within a single program
+my %DIR_CACHE;
 
-    croak "archive '$shared_files_tarball' not found in $dir"
+sub dist_dir {
+    my $dist = shift;
+
+    return $DIR_CACHE{$dist} if $DIR_CACHE{$dist};
+
+    my $dir = File::ShareDir::dist_dir($dist);
+
+    # no tarball? Assume regular shared dir
+    return $DIR_CACHE{$dist} = $dir 
         unless -f "$dir/$shared_files_tarball";
 
     my $archive = Archive::Tar->new;
@@ -47,8 +56,26 @@ sub dist_dir {
 
     $archive->extract;
 
-    return $tmpdir;
+    return $DIR_CACHE{$dist} = $tmpdir;
 }
+
+sub dist_file {
+    my $dist = File::ShareDir::_DIST(shift);
+    my $file = File::ShareDir::_FILE(shift);
+
+    my $path = dist_dir($dist).'/'.$file;
+
+	return undef unless -e $path;
+
+    croak("Found dist_file '$path', but not a file") 
+        unless -f $path;
+
+    croak("File '$path', no read permissions") 
+        unless -r $path;
+
+	return $path;
+}
+
 
 1;
 
@@ -62,11 +89,11 @@ File::ShareDir::Tarball - Deal transparently with shared files distributed as ta
 
 =head1 VERSION
 
-version 0.1.0
+version 0.2.0
 
 =head1 SYNOPSIS
 
-    use File::ShareDir::Tarball ':ALL';
+    use File::ShareDir::Tarball ':all';
 
     # use exactly like File::ShareDir
     $dir = dist_dir('File-ShareDir');
@@ -77,11 +104,36 @@ If the shared files of a distribution are contained in a
 tarball (see L<Dist::Zilla::Plugin::ShareDir::Tarball> for
 why you would want to do that), automatically 
 extract the archive in a temporary
-directory and return the path to that directory. In other words,
+directory and return the path to that directory. If called for a regular distribution without a bundle file
+(C<shared-files.tar.gz>), it'll return the original shared dir. 
+In other words,
 from the consumer point of view, it'll behave just like L<File::ShareDir>.
 
-NOTE: for now, only C<dist_dir()> is supported. The other functions will soon
-come.
+=head1 EXPORT TAGS
+
+=head2 :all
+
+Exports C<dist_dir()> and C<dist_file()>.
+
+=head1 EXPORTABLE FUNCTIONS
+
+=head2 dist_dir( $distribution )
+
+Behaves just like C<dist_dir()> from L<File::ShareDir>.
+
+=head2 dist_file( $distribution, $file )
+
+Behaves just like C<dist_file()> from L<File::ShareDir>.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Test::File::ShareDir>
+
+To test or use a shared dir that is not deployed yet. 
+
+=back
 
 =head1 AUTHOR
 
